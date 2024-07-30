69c63035665bd890223f5fc7f4df26d82497a92a
Marcel Bosse <marcel.bosse@tensora.co>
2024-07-30 22:58:16 +0200

    added length values to heatmap

--- viz.py
+++ viz.py
@@ -1,8 +1,9 @@
 import folium
 import numpy as np
-import polars as pl
-import plotly.io as pio
 import plotly.express as px
+import plotly.io as pio
+import polars as pl
+
 
 def icon(text: str):
     return f"""
@@ -46,61 +47,67 @@ def create_map(counts, project):
     # Save the map to an HTML file
     m.save("static/map.html")
 
+
 def line_chart(df: pl.DataFrame, areas: set[str]):
     df = df.melt(id_vars=["timestamp"], variable_name="area", value_name="count")
     df = df.filter(pl.col("area").is_in(areas))
-    
+
     fig = px.area(
         df.to_pandas(),
         x="timestamp",
         y="count",
         color="area",
         line_shape="spline",
-        height=200
+        height=200,
     )
-    
+
     fig.update_xaxes(
         tickformat="%H:%M",
         nticks=10,
         title_text=None,  # Hide x-axis label
-        tickfont=dict(color='#808080'),  # Medium gray for tick labels
-        gridcolor='#808080'  # Medium gray for grid lines
+        tickfont=dict(color="#808080"),  # Medium gray for tick labels
+        gridcolor="#808080",  # Medium gray for grid lines
     )
-    
+
     fig.update_yaxes(
         title_text=None,
-        tickfont=dict(color='#808080'),  # Medium gray for tick labels
-        gridcolor='#808080'  # Medium gray for grid lines
+        tickfont=dict(color="#808080"),  # Medium gray for tick labels
+        gridcolor="#808080",  # Medium gray for grid lines
     )
 
     fig.update_layout(
         margin=dict(l=0, r=0, t=0, b=0),  # Minimal margins
-        legend_title_text='',
-        paper_bgcolor='rgba(0,0,0,0)',  # Transparent background
-        plot_bgcolor='rgba(0,0,0,0)',   # Transparent plot area
-        legend=dict(
-            font=dict(color='#808080')  # Medium gray for legend text
-        )
+        legend_title_text="",
+        paper_bgcolor="rgba(0,0,0,0)",  # Transparent background
+        plot_bgcolor="rgba(0,0,0,0)",  # Transparent plot area
+        legend=dict(font=dict(color="#808080")),  # Medium gray for legend text
     )
-    return pio.to_html(fig, full_html=False, include_plotlyjs=False, config={'displayModeBar': False})
+    return pio.to_html(
+        fig, full_html=False, include_plotlyjs=False, config={"displayModeBar": False}
+    )
+
 
 def heatmap_chart(array, crop):
     l, t, r, b = crop
     fig = px.imshow(
         array,
-        x=np.linspace(l, r-0.5, array.shape[1]),
-        y=np.linspace(b-0.5, t, array.shape[0]),
+        x=np.linspace(l, r - 0.5, array.shape[1]),
+        y=np.linspace(b - 0.5, t, array.shape[0]),
         color_continuous_scale="viridis",
         origin="lower",
         aspect="equal",
-        labels={"color": "density"}
+        labels={"color": "density"},
     )
     fig.update_layout(
         margin=dict(l=0, r=0, t=0, b=0),  # Minimal margins
-        paper_bgcolor='rgba(0,0,0,0)',  # Transparent background
-        plot_bgcolor='rgba(0,0,0,0)',  # Transparent plot area
-        font=dict(color='#808080'),  # Medium gray font color
-        xaxis=dict(title=None, showticklabels=False, showgrid=False),
-        yaxis=dict(title=None, showticklabels=False, showgrid=False)
+        paper_bgcolor="rgba(0,0,0,0)",  # Transparent background
+        plot_bgcolor="rgba(0,0,0,0)",  # Transparent plot area
+        font=dict(color="#808080"),  # Medium gray font color
+        # xaxis=dict(title=None, showticklabels=False, showgrid=False),
+        # yaxis=dict(title=None, showticklabels=False, showgrid=False),
+        xaxis=dict(title="Distance (meters)", showticklabels=True, showgrid=False),
+        yaxis=dict(title="Distance (meters)", showticklabels=True, showgrid=False),
     )
-    return pio.to_html(fig, full_html=False, include_plotlyjs=False, config={'displayModeBar': False})
+    return pio.to_html(
+        fig, full_html=False, include_plotlyjs=False, config={"displayModeBar": False}
+    )
